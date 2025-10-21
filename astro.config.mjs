bb255a4d825a8a13893a0f4459c0865506b446e1
Miguel Terán <miguel.teranj02@gmail.com>
2025-10-21 19:47:44 -0400

    feat(navbar): Añadir elementos hijo al componente Navbar
    - Añadir Botones y menú responsivo para la interacción con mi información
    - Añadir Botones y menú responsivo para la configuración de accesibilidad del usuario.
    - Añadir Input para la barra de búsqueda de proyectos, artículos, etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    icon({
+      include: {
+        "line-md": ["github", "linkedin", "download-loop"],
+        "material-symbols": [
+          "translate-rounded",
+          "light-mode-rounded",
+          "dark-mode-rounded",
+          "accessible-menu-sharp",
+          "app-registration-outline",
+        ],
+      },
+    }),
+  ],
 });
